b5e60a3c0c6eb2b6ef4dc61d052ad7aec419a04f
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2021-12-18 23:52:43 -0600

    Added comments for tomorrow's plan.

--- dadjokeapp/src/App.js
+++ dadjokeapp/src/App.js
@@ -3,6 +3,12 @@ import Navbar from './Navbar'
 import Header from './Header'
 import AllJokes from './AllJokes'
 
+//Icorporate random joke from api
+//move "all jokes" component into it's own page. Perhaps when you click "all jokes",
+//change the text of the h1 to "Al jokes" and render the "all-jokes" div under it.
+//Home ONLY has joke of the day.
+
+
 function App() {
   return (
     <div className="app">
